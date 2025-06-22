86d80a29828a04bdc7765da98d17dee7aeec4d71
Danil Sergeev <poza.voina@gmail.com>
2025-06-22 21:00:14 +0300

    patch

--- minio_setup.sh
+++ minio_setup.sh
@@ -6,11 +6,14 @@ mc alias set local http://minio:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 # Создаем bucket (если ещё не создан)
 mc mb local/attachments || true
 
+# выдаем PUBLIC доступ ^_^
+mc anonymous set download local/attachments || true
+
 # Создаем пользователя (если ещё не создан)
 mc admin user add local appuser apppassword || true
 
 # Добавляем политику (если ещё не добавлена)
-mc admin policy create local appolicy /scripts/minio-policy.json || true
+mc admin policy create local appolicy ./scripts/minio-policy.json || true
 
 # Назначаем политику пользователю
 mc admin policy attach local appolicy --user appuser
