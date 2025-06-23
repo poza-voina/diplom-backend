4b15efabcb26b44023e234e44e50e0f09f8a66f0
Danil Sergeev <poza.voina@gmail.com>
2025-06-23 21:32:43 +0300

    migration

---
+++ src/Infrastructure/Migrations/20250623183222_Migration4.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "admin",
+                keyColumn: "id",
+                keyValue: 1L,
+                column: "email",
+                value: "admin@tourguide.site");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "admin",
+                keyColumn: "id",
+                keyValue: 1L,
+                column: "email",
+                value: "admin@digitaldiary.site");
+        }
+    }
+}
